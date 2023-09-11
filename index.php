c127c4745afdbececb48c9560b51c78bc60e2f22
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2023-09-11 10:36:55 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMV - Estoque</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: sans-serif;
+        }
+        header {
+            background-image: url('https://www.comprarroupasatacado.com.br/blog/wp-content/uploads/2018/08/Aimportanciadagestaodeestoquenasualojaderoupas.jpeg');
+            background-size: cover;
+            height: 30vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        header h1 {
+            color: white;
+            font-weight: bold;
+            font-size: 4em;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>CMV</h1>
+    </header>
+    <?php
+        include "conection.php";
+        
+    ?>
+</body>
+</html>
